82e338030ee488d417628333f31cb50672094055
Milan Kriz <milan.kriz@eccam.com>
2022-01-28 15:29:16 +0100

    Allow to upload complex schema as a zip file

--- interactive_zserio.py
+++ interactive_zserio.py
@@ -21,23 +21,14 @@ def get_package_names(zs):
         return m.group(1).split(".")
     return ["default"]
 
-def compile(zs, gen_dir, langs, extra_args):
-    if not len(zs):
-        return False
-
-    package_names = get_package_names(zs)
-    zs_path = os.path.join(gen_dir, "zs")
-    zs_file_path = os.path.join(*package_names[0:-1], package_names[-1] + ".zs")
-    os.makedirs(os.path.join(zs_path, *package_names[0:-1]))
-    with open(os.path.join(zs_path, zs_file_path), "w") as zs_file:
-        zs_file.write(zs)
-
+def compile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
     args=[]
     args+=extra_args
-    args+=["-src", zs_path]
-    args.append(zs_file_path)
-    for lang in langs:
-        args+=["-" + lang, os.path.join(gen_dir, lang)]
+    args+=["-src", zs_dir]
+    args.append(root_zs_file_path)
+    for lang, checked in langs.items():
+        if checked:
+            args+=["-" + lang, os.path.join(gen_dir, lang)]
 
     completed_process = zserio.run_compiler(args)
     if completed_process.returncode != 0:
@@ -59,18 +50,21 @@ def compress(gen_dir, zip_filename):
         for f in files_to_zip:
             zip_file.write(f)
 
-def recompile_params_changed(zs, checked_langs, extra_args):
-    recompile_params = (zs, checked_langs, extra_args)
+def recompile_params_changed(zs_dir, root_zs_file_path, checked_langs, extra_args):
+    with open(os.path.join(zs_dir, root_zs_file_path), "r") as root_zs_file:
+        recompile_params = (root_zs_file.read(), checked_langs, extra_args)
+
     if not "recompile_params" in st.session_state or st.session_state.recompile_params != recompile_params:
         st.session_state.recompile_params = recompile_params
         return True
     return False
 
-def recompile(zs, gen_dir, checked_langs, extra_args):
-    if recompile_params_changed(zs, checked_langs, extra_args):
-        shutil.rmtree(gen_dir, ignore_errors=True)
+def recompile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
+    if recompile_params_changed(zs_dir, root_zs_file_path, langs, extra_args):
+        for lang in langs:
+            shutil.rmtree(os.path.join(gen_dir, lang), ignore_errors=True)
         with st.spinner("Compiling..."):
-            if not compile(zs, gen_dir, checked_langs, extra_args):
+            if not compile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
                 st.stop()
         return True
     else:
@@ -99,47 +93,85 @@ def add_zip_download(zip_path):
     with open(zip_path, "rb") as zip_file:
         st.download_button("Download sources", zip_file, mime="application/zip")
 
+def upload_schema(zs_dir, uploaded_schema):
+    if uploaded_schema.type == "application/zip":
+        #complex schema within the zip file - just unpack in the zs_dir directory
+        shutil.rmtree(zs_dir, ignore_errors=True)
+        schema_files = []
+        with ZipFile(uploaded_schema, "r") as zip_file:
+            zip_file.extractall(zs_dir)
+        for root, dirs, files in os.walk(zs_dir):
+            for f in files:
+                schema_files.append(os.path.relpath(os.path.join(root, f), zs_dir))
+        return st.selectbox("Choose root file", schema_files)
+    else:
+        # simple schema - process content in case that the schema file contains complex package name
+        schema = uploaded_schema.getvalue().decode("utf8")
+        return process_simple_schema(schema, zs_dir)
+
+def process_simple_schema(schema, zs_dir):
+    zs = st.text_area("Zserio Schema", value=schema, height=150)
+    if not zs.strip():
+        return None
+
+    package_names = get_package_names(zs)
+    root_zs_file_path = os.path.join(*package_names[0:-1], package_names[-1] + ".zs")
+    shutil.rmtree(zs_dir, ignore_errors=True)
+    os.makedirs(os.path.join(zs_dir, *package_names[0:-1]))
+    with open(os.path.join(zs_dir, root_zs_file_path), "w") as zs_file:
+        zs_file.write(zs)
+    return root_zs_file_path
+
 st.write("""
 # Interactive Zserio Compiler!
 """)
 
+gen_dir = "gen"
+zs_dir = os.path.join(gen_dir, "zs")
+zip_filename = "gen.zip"
+
 st.session_state.sample_mode = False
 
-uploaded_schema = st.file_uploader("Upload schema", type="zs")
+upload_help="Upload either a simple schema file *.zs, or complex schema as a *.zip."
+uploaded_schema = st.file_uploader("Upload schema", type=["zs","zip"], help=upload_help)
 if uploaded_schema:
     st.session_state.sample_mode = False
-    schema = uploaded_schema.getvalue().decode("utf8")
+    root_zs_file_path = upload_schema(zs_dir, uploaded_schema)
     uploaded_schema.close()
 else:
     with open("sample_src/sample.zs", "r") as sample:
         st.session_state.sample_mode = True
         schema = sample.read()
-
-zs = st.text_area("Zserio Schema", value=schema, height=150)
+        root_zs_file_path = process_simple_schema(schema, zs_dir)
 
 extra_args = st.text_input("Extra Arguments").split()
 
-langs = (
-    "python", "cpp", "java", "xml", "doc"
-)
+langs = {
+    "python": True,
+    "cpp": False,
+    "java": False,
+    "xml": False,
+    "doc" : False
+}
 
 langs_cols = st.columns(len(langs))
 
 langs_checks = []
-for i, lang in enumerate(langs):
+for i, (lang, lang_enabled) in enumerate(langs.items()):
     with langs_cols[i]:
-        langs_checks.append(st.checkbox(lang, value=(lang == "python")))
+        langs_checks.append(st.checkbox(lang, value=lang_enabled))
 
-checked_langs = []
-for i, checked in enumerate(langs_checks):
-    if checked:
-        checked_langs.append(langs[i])
+for i, lang in enumerate(langs):
+    langs[lang] = langs_checks[i]
 
-gen_dir = "gen"
-zip_filename = "gen.zip"
-if recompile(zs, gen_dir, checked_langs, extra_args):
+if not root_zs_file_path:
+    st.info("No schema provided...")
+    st.stop()
+
+if recompile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
     compress(gen_dir, zip_filename)
 
+checked_langs = [lang for lang in langs if langs[lang]]
 if len(checked_langs):
     cols = st.columns(len(checked_langs))
     for i, lang in enumerate(checked_langs):
@@ -166,6 +198,7 @@ if python_code_check and "python" in checked_langs:
         py_code = ""
 
     py_code = st.text_area("Python code", value=py_code, height=250)
+
     if st.session_state.sample_mode:
         st.session_state.py_sample_code = py_code
 
