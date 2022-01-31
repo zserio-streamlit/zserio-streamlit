b8d2b37a6dd8c41a47d6942ca1975c7bc7a6102f
Milan Kriz <milan.kriz@eccam.com>
2022-01-31 12:28:31 +0100

    Redesign UI, allow to edit multiple schema files

--- interactive_zserio.py
+++ interactive_zserio.py
@@ -1,9 +1,11 @@
 import os
-import sys
 import re
-import io
 import shutil
+import sys
 import streamlit as st
+import zserio
+
+from datetime import datetime
 from glob import glob
 from zipfile import ZipFile
 
@@ -11,24 +13,152 @@ from zipfile import ZipFile
 from io import StringIO
 from contextlib import redirect_stdout, redirect_stderr
 
-import zserio
-
 st.set_page_config(layout="wide", page_title="Interactive Zserio", page_icon="./img/zs.png")
 
-def get_package_names(zs):
-    m = re.search("package (.*);", zs)
-    if m:
-        return m.group(1).split(".")
-    return ["default"]
+def init_session_variable(name, value=None):
+    if name not in st.session_state:
+        st.session_state[name] = value
 
-def compile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
+init_session_variable("schema_mode_changed", True)
+init_session_variable("upload_schema_changed", False)
+init_session_variable("zs_code", None)
+init_session_variable("selected_file", None)
+init_session_variable("recompile_params", None)
+init_session_variable("py_code", None)
+
+def log(*args):
+    print(datetime.now().strftime("%H:%M:%S.%f: "), *args, file=sys.stderr)
+
+def schema_mode_on_change():
+    st.session_state.zs_code = None
+    st.session_state.selected_file = None
+    st.session_state.recompile_params = None
+    st.session_state.py_code = None
+
+    st.session_state.schema_mode_changed = True
+
+
+def cleanup_workspace(ws_dir, zs_dir):
+    shutil.rmtree(ws_dir, ignore_errors=True)
+    os.makedirs(zs_dir)
+
+def write_schema(ws_dir, zs_dir):
+    if not st.session_state.schema_mode_changed:
+        return
+
+    cleanup_workspace(ws_dir, zs_dir)
+
+def upload_schema_on_change():
+    st.session_state.upload_schema_changed = True
+
+def upload_schema(ws_dir, zs_dir):
+    upload_help="Upload either a simple schema file *.zs, or complex schema as a *.zip."
+    uploaded_schema = st.file_uploader("Upload schema", type=["zs","zip"], help=upload_help,
+                                       on_change=upload_schema_on_change)
+    if not st.session_state.schema_mode_changed and not st.session_state.upload_schema_changed:
+        return
+
+    if not uploaded_schema:
+        cleanup_workspace(ws_dir, zs_dir)
+        st.stop()
+
+    if st.session_state.upload_schema_changed:
+        cleanup_workspace(ws_dir, zs_dir)
+        with ZipFile(uploaded_schema, "r") as zip_file:
+            zip_file.extractall(zs_dir)
+        st.session_state.upload_schema_changed = False
+
+def sample_schema(ws_dir, zs_dir):
+    if not st.session_state.schema_mode_changed:
+        return
+
+    cleanup_workspace(ws_dir, zs_dir)
+    shutil.copy("sample_src/sample.zs", zs_dir)
+
+def list_schema_files(folder):
+    listed_files = []
+    for root, _, files in os.walk(folder):
+        for listed_file in files:
+            if listed_file.endswith(".zs"):
+                listed_files.append(os.path.relpath(os.path.join(root, listed_file), folder))
+    listed_files.sort()
+    return listed_files
+
+def create_new_file(zs_dir):
+    file_path = st.text_input("Choose file path", help="Enter relative path of the schema file to create.")
+    if not file_path:
+        st.stop()
+
+    if not file_path.endswith(".zs"):
+        st.error("Extension must be *.zs")
+        st.stop()
+
+    file_full_path = os.path.join(zs_dir, file_path)
+    path, file_name = os.path.split(file_full_path)
+    os.makedirs(path, exist_ok=True)
+    with open(file_full_path, "w") as new_file:
+        package_definition = ".".join(os.path.splitext(file_path)[0].split(os.sep))
+        new_file.write(f"package {package_definition};\n")
+
+    st.session_state.zs_code = None
+    return file_path
+
+def choose_file_on_change():
+    st.session_state.zs_code = None
+
+def selected_file_index(schema_files):
+    if st.session_state.selected_file is None:
+        return 0
+
+    index = schema_files.index(st.session_state.selected_file)
+    log("index:", st.session_state.selected_file, index)
+    st.session_state.selected_file = None
+    return index
+
+def choose_file(zs_dir):
+    schema_files = list_schema_files(zs_dir)
+    if schema_files:
+        index = selected_file_index(schema_files)
+
+        create_new = "Create new..."
+        schema_files.append(create_new)
+
+        cols = st.columns([7, 1])
+        choose_help="Choose file to edit. The selected file will be used as root schema file for compilation."
+        selected_file = cols[0].selectbox("Choose schema file", options=schema_files, index=index,
+                                          on_change=choose_file_on_change, help=choose_help)
+        cols[1].title("")
+        remove_file = cols[1].button("❌ Delete", disabled=(selected_file == create_new),
+                                     help="Remove the selected file.")
+        if remove_file:
+            os.remove(os.path.join(zs_dir, selected_file))
+            st.experimental_rerun()
+
+    if not schema_files or selected_file == create_new:
+        st.session_state.selected_file = create_new_file(zs_dir)
+        st.experimental_rerun()
+
+    return selected_file
+
+def edit_file(zs_dir, file_path):
+    if st.session_state.zs_code is None:
+        log("read schema file:", file_path)
+        with open(os.path.join(zs_dir, file_path), "r") as f:
+            st.session_state.zs_code = f.read()
+
+    new_content = st.text_area(file_path, height=250, key="zs_code")
+    with open(os.path.join(zs_dir, file_path), "w") as f:
+        log("write schema file:", file_path)
+        f.write(new_content)
+
+def compile(zs_dir, root_zs_file_path, gen_dir, generators, extra_args):
     args=[]
     args+=extra_args
     args+=["-src", zs_dir]
     args.append(root_zs_file_path)
-    for lang, checked in langs.items():
+    for generator, checked in generators.items():
         if checked:
-            args+=["-" + lang, os.path.join(gen_dir, lang)]
+            args+=["-" + generator, os.path.join(gen_dir, generator)]
 
     completed_process = zserio.run_compiler(args)
     if completed_process.returncode != 0:
@@ -37,116 +167,89 @@ def compile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
 
     return True
 
-def compress(gen_dir, zip_filename):
-    zip_path = os.path.join(gen_dir, zip_filename)
+def recompile_params_changed(zs_dir, root_zs_file_path, generators, extra_args):
+    with open(os.path.join(zs_dir, root_zs_file_path), "r") as root_zs_file:
+        recompile_params = (root_zs_file_path, root_zs_file.read(), generators, extra_args)
+
+    if st.session_state.recompile_params != recompile_params:
+        st.session_state.recompile_params = recompile_params
+        return True
+    return False
+
+def recompile(zs_dir, root_zs_file_path, gen_dir, generators, extra_args):
+    if recompile_params_changed(zs_dir, root_zs_file_path, generators, extra_args):
+        for generator in generators:
+            shutil.rmtree(os.path.join(gen_dir, generator), ignore_errors=True)
+        with st.spinner("Compiling..."):
+            if not compile(zs_dir, root_zs_file_path, gen_dir, generators, extra_args):
+                st.stop()
+    else:
+        st.info("No recompilation needed")
+
+def create_zip(ws_dir, zip_filename):
+    zip_path = os.path.join(ws_dir, zip_filename)
     if os.path.exists(zip_path):
         os.remove(zip_path)
 
     files_to_zip = []
-    for root, dirs, files in os.walk(gen_dir):
+    for root, dirs, files in os.walk(ws_dir):
         for f in files:
             files_to_zip.append(os.path.join(root, f))
     with ZipFile(zip_path, "w") as zip_file:
         for f in files_to_zip:
             zip_file.write(f)
 
-def recompile_params_changed(zs_dir, root_zs_file_path, checked_langs, extra_args):
-    with open(os.path.join(zs_dir, root_zs_file_path), "r") as root_zs_file:
-        recompile_params = (root_zs_file.read(), checked_langs, extra_args)
-
-    if not "recompile_params" in st.session_state or st.session_state.recompile_params != recompile_params:
-        st.session_state.recompile_params = recompile_params
-        return True
-    return False
-
-def recompile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
-    if recompile_params_changed(zs_dir, root_zs_file_path, langs, extra_args):
-        for lang in langs:
-            shutil.rmtree(os.path.join(gen_dir, lang), ignore_errors=True)
-        with st.spinner("Compiling..."):
-            if not compile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
-                st.stop()
-        return True
-    else:
-        st.info("No recompilation needed...")
-        return False
-
-def map_highlighting(lang):
-    if lang == "doc":
+def map_highlighting(generator):
+    if generator == "doc":
         return "html"
-    if lang == "cpp":
-        # see: https://discuss.streamlit.io/t/c-markdown-syntax-highlighting-doesnt-work/14106/2
-        # streamlit seems to have problems with c++ / cpp / cxx
-        return "c"
-    return lang
+    return generator
 
-def display_sources(gen_dir, lang):
-    st.caption(lang)
-    generated = glob(gen_dir + "/" + lang + "/**", recursive=True)
+def display_sources(gen_dir, generator):
+    st.caption(generator)
+    generated = glob(gen_dir + "/" + generator + "/**", recursive=True)
     for gen in generated:
         if os.path.isfile(gen):
             with open(gen, "r") as source:
                 with st.expander(gen):
-                    st.code(source.read(), map_highlighting(lang))
+                    st.code(source.read(), map_highlighting(generator))
 
 def add_zip_download(zip_path):
     with open(zip_path, "rb") as zip_file:
-        st.download_button("Download sources", zip_file, mime="application/zip")
-
-def upload_schema(zs_dir, uploaded_schema):
-    if uploaded_schema.type == "application/zip":
-        #complex schema within the zip file - just unpack in the zs_dir directory
-        shutil.rmtree(zs_dir, ignore_errors=True)
-        schema_files = []
-        with ZipFile(uploaded_schema, "r") as zip_file:
-            zip_file.extractall(zs_dir)
-        for root, dirs, files in os.walk(zs_dir):
-            for f in files:
-                schema_files.append(os.path.relpath(os.path.join(root, f), zs_dir))
-        return st.selectbox("Choose root file", schema_files)
-    else:
-        # simple schema - process content in case that the schema file contains complex package name
-        schema = uploaded_schema.getvalue().decode("utf8")
-        return process_simple_schema(schema, zs_dir)
-
-def process_simple_schema(schema, zs_dir):
-    zs = st.text_area("Zserio Schema", value=schema, height=150)
-    if not zs.strip():
-        return None
-
-    package_names = get_package_names(zs)
-    root_zs_file_path = os.path.join(*package_names[0:-1], package_names[-1] + ".zs")
-    shutil.rmtree(zs_dir, ignore_errors=True)
-    os.makedirs(os.path.join(zs_dir, *package_names[0:-1]))
-    with open(os.path.join(zs_dir, root_zs_file_path), "w") as zs_file:
-        zs_file.write(zs)
-    return root_zs_file_path
+        st.download_button("Download workspace", zip_file, mime="application/zip",
+                           help="Download whole workspace as a zip file.")
 
 st.write("""
 # Interactive Zserio Compiler!
 """)
 
-gen_dir = "gen"
-zs_dir = os.path.join(gen_dir, "zs")
-zip_filename = "gen.zip"
+ws_dir = "workspace"
+zs_dir = os.path.join(ws_dir, "zs")
+gen_dir = os.path.join(ws_dir, "gen")
+src_dir = os.path.join(ws_dir, "src")
+code_filename = "code.py"
+zip_filename = "all.zip"
 
-st.session_state.sample_mode = False
-
-upload_help="Upload either a simple schema file *.zs, or complex schema as a *.zip."
-uploaded_schema = st.file_uploader("Upload schema", type=["zs","zip"], help=upload_help)
-if uploaded_schema:
-    st.session_state.sample_mode = False
-    root_zs_file_path = upload_schema(zs_dir, uploaded_schema)
-    uploaded_schema.close()
+schema_modes = { "write": "Write schema", "upload": "Upload schema", "sample": "Sample" }
+schema_mode = st.selectbox("Schema", schema_modes, format_func=lambda x: schema_modes[x], index=2,
+                           on_change=schema_mode_on_change)
+if schema_mode == "write":
+    write_schema(ws_dir, zs_dir)
+elif schema_mode == "upload":
+    upload_schema(ws_dir, zs_dir)
+elif schema_mode == "sample":
+    sample_schema(ws_dir, zs_dir)
 else:
-    with open("sample_src/sample.zs", "r") as sample:
-        st.session_state.sample_mode = True
-        schema = sample.read()
-        root_zs_file_path = process_simple_schema(schema, zs_dir)
+    log("unknown schema mode!", schema_mode)
+    st.error("Unknown schema mode!")
+st.session_state.schema_mode_changed = False
+
+zs_root_file_path = choose_file(zs_dir)
+
+edit_file(zs_dir, zs_root_file_path)
 
 extra_args = st.text_input("Extra Arguments").split()
 
-langs = {
+generators = {
     "python": True,
     "cpp": False,
     "java": False,
@@ -154,34 +257,36 @@ langs = {
     "doc" : False
 }
 
-langs_cols = st.columns(len(langs))
+generators_cols = st.columns(len(generators))
 
-langs_checks = []
-for i, (lang, lang_enabled) in enumerate(langs.items()):
-    with langs_cols[i]:
-        langs_checks.append(st.checkbox(lang, value=lang_enabled))
+generators_checks = []
+for i, (generator, generator_enabled) in enumerate(generators.items()):
+    with generators_cols[i]:
+        generators_checks.append(st.checkbox(generator, value=generator_enabled))
 
-for i, lang in enumerate(langs):
-    langs[lang] = langs_checks[i]
+for i, generator in enumerate(generators):
+    generators[generator] = generators_checks[i]
 
-if not root_zs_file_path:
-    st.info("No schema provided...")
-    st.stop()
+recompile(zs_dir, zs_root_file_path, gen_dir, generators, extra_args)
 
-if recompile(zs_dir, root_zs_file_path, gen_dir, langs, extra_args):
-    compress(gen_dir, zip_filename)
-
-checked_langs = [lang for lang in langs if langs[lang]]
-if len(checked_langs):
-    cols = st.columns(len(checked_langs))
-    for i, lang in enumerate(checked_langs):
+checked_generators = [generator for generator in generators if generators[generator]]
+if len(checked_generators):
+    cols = st.columns(len(checked_generators))
+    for i, generator in enumerate(checked_generators):
         with cols[i]:
-            display_sources(gen_dir, lang)
-    add_zip_download(os.path.join(gen_dir, zip_filename))
+            display_sources(gen_dir, generator)
 
 python_code_check = st.checkbox("Experimental python code", help="Python generator must be enabled", value=True)
 
-if python_code_check and "python" in checked_langs:
+if python_code_check and generators["python"]:
+    os.makedirs(src_dir, exist_ok=True)
+    code_file_path = os.path.join(src_dir, code_filename)
+    if not os.path.exists(code_file_path):
+        if schema_mode == "sample":
+            shutil.copy(os.path.join("sample_src", "sample.py"), code_file_path)
+        else:
+            open(code_file_path, "w").close()
+
     sys.dont_write_bytecode = True
     gen_path = os.path.join(gen_dir, "python")
     if sys.path[-1] != gen_path:
@@ -189,23 +294,20 @@ if python_code_check and "python" in checked_langs:
 
     modules_keys = set(sys.modules.keys())
 
-    if st.session_state.sample_mode:
-        if not "py_sample_code" in st.session_state:
-            with open("sample_src/sample.py", "r") as sample:
-                st.session_state.py_sample_code = sample.read()
-        py_code = st.session_state.py_sample_code
-    else:
-        py_code = ""
+    if st.session_state.py_code is None:
+        with open(code_file_path, "r") as code_file:
+            log("read code file:", code_file_path)
+            st.session_state.py_code = code_file.read()
 
-    py_code = st.text_area("Python code", value=py_code, height=250)
-
-    if st.session_state.sample_mode:
-        st.session_state.py_sample_code = py_code
+    new_py_code = st.text_area("Python code", height=250, key="py_code")
+    with open(code_file_path, "w") as code_file:
+        log("write code file:", code_file_path)
+        code_file.write(new_py_code)
 
     with StringIO() as out, redirect_stdout(out):
         st.caption("Python output")
         try:
-            exec(py_code)
+            exec(new_py_code)
             st.text(out.getvalue())
         except Exception as e:
             st.error(e)
@@ -215,3 +317,6 @@ if python_code_check and "python" in checked_langs:
     modules_to_remove = new_modules_keys - modules_keys
     for module_to_remove in modules_to_remove:
         sys.modules.pop(module_to_remove)
+
+create_zip(ws_dir, zip_filename)
+add_zip_download(os.path.join(ws_dir, zip_filename))
